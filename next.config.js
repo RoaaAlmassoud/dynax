65087ab761593c6bde98ddac350abfbca0efd46d
RoaaAlmassoud <ro2a.almasoud@gmail.com>
2023-02-13 17:48:20 +0300

    final-commit

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   swcMinify: true,
   // distDir: 'build',
   // basePath: '/users/test',
- basePath: '/users/bonz',
+//  basePath: '/users/bonz',
 eslint: {
     ignoreDuringBuilds: true
   },

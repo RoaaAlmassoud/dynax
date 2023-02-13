27106e66530c0dc88375d0f960169295d94cfa6b
root <root@ip-10-0-5-40.ap-northeast-1.compute.internal>
2023-02-13 13:47:08 +0000

    changeg readme and config

--- next.config.js
+++ next.config.js
@@ -4,11 +4,12 @@ const nextConfig = {
   swcMinify: true,
   // distDir: 'build',
   // basePath: '/users/test',
-  eslint: {
+ basePath: '/users/bonz',
+eslint: {
     ignoreDuringBuilds: true
   },
  env:{
-     IMAGE_URL: process.env.IMAGE_URL || "",
+     IMAGE_URL: process.env.IMAGE_URL || "https://hoyojo-new.dynax.co.jp/users/bonz",
      API_URL: process.env.API_URL || "https://hoyojo-new.dynax.co.jp/api/"
    }
 }
